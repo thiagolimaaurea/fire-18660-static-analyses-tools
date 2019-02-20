c6be960f0b6f17a68b01568c080966694087c523
Thiago Lima <thiago.lima@aurea.com>
2019-02-20 15:40:28 -0800

    First project commit

--- src/main/java/com/aurea/HelloWorld.java
+++ src/main/java/com/aurea/HelloWorld.java
@@ -1,8 +1,12 @@
-
+package com.aurea;
 
 public class HelloWorld {
 
+    // UWF_UNWRITTEN_PUBLIC_OR_PROTECTED_FIELD
+    // MS malicious code
+    public static Integer a;
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World!" + a);
     }
 }
